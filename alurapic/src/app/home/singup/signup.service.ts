2225f755340ed9ceb8f7a74141e33c3596f3adff
Richard Rosário <richard.rosario@autoglass.com.br>
2020-05-28 10:24:24 -0300

    Realiza pequenas alterações

--- alurapic/src/app/home/singup/signup.service.ts
+++ alurapic/src/app/home/singup/signup.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NewUser } from './new-user';
-import { provideForRootGuard } from '@angular/router/src/router_module';
 
 import { environment } from '../../../environments/environment';
 
